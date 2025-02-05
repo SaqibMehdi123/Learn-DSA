74725faa25220963bfc2cdc05d0b4896a0d1a2e2
SaqibMehdi123 <saqibmehdi234@gmail.com>
2025-02-05 17:24:49 +0500

    add the cpp file: conditionals.cpp

--- Lecture3 (conditionals and loops)/conditionals.cpp
+++ Lecture3 (conditionals and loops)/conditionals.cpp
@@ -1,8 +1,66 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
+    int a, b;
+
+    // Ways to write the conditionals
+
+    // One way: if()
+
+    /*
+
+    cout << "Enter the value of a: ";
+    cin >> a;
+    cout << "Enter the value of b: ";
+    cin >> b;
+
+    if(a > b){
+        cout << "a is greater than b" << endl;
+    }
+    if(b > a) {
+        cout << "b is greater than a" << endl;
+    }
+
+    */
+
+
+
+    // Another way: nested conditionals
+    /*
+
+    cout << "Enter the value of a: ";
+    cin >> a;
+
+    if (a > 0) {
+        cout << "a is positive";
+    }
+    else {
+        if (a < 0) {
+            cout << "a is negative";
+        }
+        else {
+            cout << "a is zero";
+        }
+    }
+
+    */
+
+
+    // Another way: else if()
     
+    cout << "Enter the value of a: ";
+    cin >> a;
+
+    if(a > 0){
+        cout << "a is positive" << endl;
+    }
+    else if(a < 0){
+        cout << "a is negative" << endl;
+    }
+    else{
+        cout << "a is zero" << endl;
+    }
+
     return 0;
 }
