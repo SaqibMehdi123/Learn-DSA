96a68b83f29cb80bc03935fc1f8a35c78c320761
SaqibMehdi123 <saqibmehdi234@gmail.com>
2025-02-09 17:07:15 +0500

    add the file: extra.cpp

---
+++ Lecture4 (patterns)/extra.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+
+    int i = 1, count = 1;
+    while(i <= num){
+        int j = 1;
+        while(j <= i){
+            cout << count - i + 1 << " ";
+            count++;
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
