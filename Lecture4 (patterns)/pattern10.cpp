dbf7ba6b7054788c02aa3f3068096fcd30981d69
SaqibMehdi123 <saqibmehdi234@gmail.com>
2025-02-09 15:55:10 +0500

    add the file: pattern10

---
+++ Lecture4 (patterns)/pattern10.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    cout << "Enter the number: ";
+    cin >> num;
+
+    int i = 1, count = 1;
+    while(i <= num){
+        int j = 1;
+        count = i;
+        while (j <= i) {
+            cout << count - j + 1 << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
