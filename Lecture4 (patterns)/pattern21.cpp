c97283623b2f2333096fe553a6932cfaf78bac3c
SaqibMehdi123 <saqibmehdi234@gmail.com>
2025-02-13 18:40:58 +0500

    add the file: pattern21.cpp

---
+++ Lecture4 (patterns)/pattern21.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    int i = n;
+    while(i >= 1) {
+        int space = 1;
+        while(space <= n - i){
+            cout << " ";
+            space++;
+        }
+        int j = 1;
+        while(j <= i) {
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i--;
+    }
+}
