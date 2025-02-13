cf3f057f22633e7fce84ae5c7e0003411fd74d33
SaqibMehdi123 <saqibmehdi234@gmail.com>
2025-02-13 19:23:25 +0500

    add the file: pattern24.cpp

---
+++ Lecture4 (patterns)/pattern24.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    int i = n;
+    while(i >= 1) {
+        int count = n - i + 1;
+        int space = 1;
+        while(space <= n - i){
+            cout << " ";
+            space++;
+        }
+        int j = 1;
+        while(j <= i) {
+            cout << count + j - 1;
+            j++;
+        }
+        cout << endl;
+        i--;
+    }
+}
