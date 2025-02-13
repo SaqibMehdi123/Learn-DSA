448449884bed10974db3eefd6d7ba89df5ad2150
SaqibMehdi123 <saqibmehdi234@gmail.com>
2025-02-13 10:03:34 +0500

    add the file: pattern12.cpp

---
+++ Lecture4 (patterns)/pattern12.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n; 
+    cout << "Enter the number: ";
+    cin >> n;
+
+    int i = 1;
+    char ch;
+    while(i <= n) {
+        ch = 'A';
+        int j = 1;
+        while(j <= n) {
+            cout << char(ch + j - 1) << " ";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    return 0;
+}
