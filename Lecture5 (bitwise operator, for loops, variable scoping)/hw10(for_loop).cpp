656851fde16d4b7198bc63ccfebbd9a879723980
SaqibMehdi123 <saqibmehdi234@gmail.com>
2025-02-16 19:21:43 +0500

    add the file: hw10(for_loop).cpp

---
+++ Lecture5 (bitwise operator, for loops, variable scoping)/hw10(for_loop).cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Output: 0 2 4
+    for(int i = 0; i <= 5; i++) {
+        cout << i << " ";
+        i++;
+    }
+
+    return 0;
+}
