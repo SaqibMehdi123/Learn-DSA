9e0887a7e5b10ac73b1802a21978ff90e6ca134b
SaqibMehdi123 <saqibmehdi234@gmail.com>
2025-02-19 19:34:49 +0500

    add ther file: for_loop(continue_keyword).cpp

---
+++ Lecture5 (bitwise operator, for loops, variable scoping)/for_loop(continue_keyword).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cout << "Enter the number: ";
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+
+        cout << " Hi " << endl;
+        cout << " Hey " << endl;
+
+        continue;
+
+        cout << " Please reply kar do na Yar" << endl;
+
+    }
+    
+    return 0;
+}
