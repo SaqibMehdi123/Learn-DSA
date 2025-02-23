e64f604411617f5985220b05173eeb8a06de1fca
SaqibMehdi123 <saqibmehdi234@gmail.com>
2025-02-23 12:06:34 +0500

    add the file: power_of_number_without_function.cpp.cpp

--- Lecture7 (switch & functions)/power_of_number_without_function.cpp
+++ Lecture7 (switch & functions)/power_of_number_without_function.cpp
@@ -2,19 +2,53 @@
 using namespace std;
 
 int main() {
-    int base, power;
+
+    // performing the same operation multiple times without using the function
+    
+    int a, b;
     cout << "Enter the base: ";
-    cin >> base;
+    cin >> a;
     cout << "Enter the power: ";
-    cin >> power;
+    cin >> b;
 
     int answer = 1;
 
-    for(int i = 0; i < power; i++) {
-        answer = answer * base;
+    for(int i = 0; i < b; i++) {
+        answer = answer * a;
     }
 
     cout << "Result: " << answer << endl;
+    cout << endl;
+
+    int c, d;
+    cout << "Enter the base: ";
+    cin >> c;
+    cout << "Enter the power: ";
+    cin >> d;
+
+    answer = 1;
+
+    for(int i = 0; i < d; i++) {
+        answer = answer * c;
+    }
+
+    cout << "Result: " << answer << endl;
+    cout << endl;
+
+    int e, f;
+    cout << "Enter the base: ";
+    cin >> e;
+    cout << "Enter the power: ";
+    cin >> f;
+
+    answer = 1;
+
+    for(int i = 0; i < f; i++) {
+        answer = answer * e;
+    }
+
+    cout << "Result: " << answer << endl;
+    cout << endl;
 
     return 0;
 }
