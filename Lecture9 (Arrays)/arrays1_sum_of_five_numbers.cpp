5becd8749872005ef3a899ffaca1d366ac1cbc09
SaqibMehdi123 <saqibmehdi234@gmail.com>
2025-02-26 23:58:01 +0500

    add the file: arrays1_sum_of_five_numbers.cpp

---
+++ Lecture9 (Arrays)/arrays1_sum_of_five_numbers.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // user-defined data type
+    // array: collection of data of similar data type
+    // contiguous (sequential) memory allocation and fixed size
+    
+    // syntax: data_type array_name[size];
+    // e.g: int arr[5];
+
+    // using index we can access any element of the array
+    // index starts from 0
+
+    // a large number of values are stored in a single variable
+    // to perform operation on large set of data
+    // e.g: find the max number in a set of 10000 numbers
+    // e.g: find the sum of all numbers in a set of 10000 numbers
+
+    int arr[5] = {1, 2, 3, 4, 5};
+
+    int sum = 0;
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    for(int i = 0; i < size; i++) {
+        sum += arr[i];
+    }
+
+    cout << "The sum of the array is: " << sum << endl;
+
+    return 0;
+}
