a339ace8e097c03febdb3e29c35294ba120e00f4
SaqibMehdi123 <saqibmehdi234@gmail.com>
2025-02-27 00:21:46 +0500

    add the file: hw1.cpp

---
+++ Lecture9 (Arrays)/hw1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5];
+    
+    for(int i = 0; i < 5; i++) {
+        arr[i] = 1;
+        cout << arr[i] << " ";
+    }
+
+    // another way to print the array in the given scenario
+
+    // for(int i = 0; i < 5; i++) {
+    //     cout << arr[i] << " ";
+    // }
+
+    return 0;
+}
