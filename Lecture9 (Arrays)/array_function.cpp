2fcbd3a5d7dad89b3c31ddf644069e474dc00560
SaqibMehdi123 <saqibmehdi234@gmail.com>
2025-03-15 15:38:13 +0500

    update the file: array_function.cpp

--- Lecture9 (Arrays)/array_function.cpp
+++ Lecture9 (Arrays)/array_function.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 void printArray(int arr[], int size) {
+    cout << "The size of the array is: " << size << endl;
+    cout << "The elements of the array are: ";
     for(int i = 0; i < size; i++) {
         cout << arr[i] << " ";
     }
@@ -9,13 +11,15 @@ void printArray(int arr[], int size) {
 
 int main() {
     int size = 15;
-    int arr1[size] = {0};
+    int arr1[size] = {1, 2, 3, 4, 5, 6, 7};
+    size = sizeof(arr1) / sizeof(int);
     printArray(arr1, size);
 
-    cout << endl;
+    cout << endl << endl;
 
     int arr2[] = {1, 2, 3, 4, 5};
-    printArray(arr2, 5);
+    size = sizeof(arr2) / sizeof(int);
+    printArray(arr2, size);
 
     return 0;
 }
